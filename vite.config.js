a5e560ee4e9ca8ca95e323eb997edd302f99905b
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-07-15 15:44:41 +0500

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // This splits your third-party node_modules packages into a separate file
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    // Optional: Slightly raise the limit to 1000kB (1MB) if you still get minor warnings
+    chunkSizeWarningLimit: 1000, 
+  }
 })
